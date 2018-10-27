3e155fca351673bde07d68cce449f464c62931a4
Leo Bright <laixinming@aisino.com>
2018-10-27 08:33:37 +0800

    按照Nicole的思路，把所有的way信息都按官方格式输出到文件里。

--- parser.py
+++ parser.py
@@ -2,17 +2,17 @@ from imposm.parser import OSMParser
 
 # simple class that handles the parsed OSM data.
 class HighwayCounter(object):
-    # highways = 0
     wayList = []
     wayCount = 0
 
     def ways(self, ways):
         # callback method for ways
         for osmid, tags, refs in ways:
-            if 'highway' in tags:
-              # self.highways += 1
-                self.wayCount += 1
-                self.wayList.append(refs)
+          # self.highways += 1
+          #   self.wayCount += 1
+          #   self.wayList.append(refs)
+            self.wayList.append((osmid, tags, refs))
+
 # instantiate counter and parser and start parsing
 counter = HighwayCounter()
 p = OSMParser(concurrency=4, ways_callback=counter.ways)
@@ -20,13 +20,11 @@ p.parse('Porto.osm.pbf')
 
 # done
 print counter.wayCount
-
 f = open(r'way_network.result', 'w+')
 
-
-for refs in counter.wayList:
-    nodes = str(refs[0]) + " " + str(refs[-1])
+for osmid, tags, refs  in counter.wayList:
+    # nodes = str(refs[0]) + " " + str(refs[-1])
+    nodes = str(osmid) + " " + str(tags) + " " + str(refs)
     print(nodes)
     f.write(nodes + '\n')
-
 f.close()
