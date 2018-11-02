9ad83132fccfe4fd27911f2d7ea4159718b28e83
leo <laixinming@aisino.com>
2018-11-02 14:53:05 +0800

    完善了ways的格式输入函数，可以根据需要来输出不同的数据

--- parser.py
+++ parser.py
@@ -6,7 +6,52 @@ class OSMCounter(object):
     coordDic = {}
     nodeDic = {}
     wayDic = {}
-    wayCount = 0
+
+    def get_node_coord(self, osmid):
+        node = self.nodeDic.get(osmid)
+        if node:
+            node_coord = node[1]
+            node_lant = node_coord[0]
+            node_lon = node_coord[1]
+        else:
+            node = self.coordDic.get(osmid)
+            if not node:
+                pass
+            else:
+                node_lant = node[0]
+                node_lon = node[1]
+        return node_lant, node_lon
+
+    # osmid : do write the way's osmid to the result file ?
+    # tag : do write the tag to the result  ?
+    # refs_index: select the index of refs you want
+    # coordinate: write the coordinate of the refs to result file or only the refs ?
+    def print_ways_result(self, output_file, osmid=False, tag=False, refs_index=[0, -1], coordinate=False):
+
+        for item in self.wayDic.items():
+            refs = item[1][-1]
+
+            # filter the way that don't match the condition
+            if len(refs) < len(refs_index):
+                continue
+
+            nodes = []
+            for index in refs_index:
+                nodes.append(refs[index])
+            if osmid:
+                output_file.write(str(item[0]) + ' ')
+            if tag:
+                output_file.write(str(item[1][0]) + ' ')
+            if coordinate:
+                for node in nodes:
+                    node_lant, node_lon = self.get_node_coord(node)
+                    output_file.write(str((node_lant, node_lon)) + ' ')
+                output_file.write('\n')
+
+            else:
+                for node in nodes:
+                    output_file.write(str(node) + ' ')
+                output_file.write('\n')
 
     def ways(self, ways):
         # callback method for ways
@@ -30,7 +75,6 @@ class OSMCounter(object):
 
 
 # instantiate counter and parser and start parsing Proto ways
-
 counter = OSMCounter()
 p = OSMParser(concurrency=4, ways_callback=counter.ways, nodes_callback=counter.nodes,
               coords_callback=counter.coords, relations_callback=counter.relations)
@@ -42,39 +86,8 @@ f_nodes = open(r'nodes.result', 'w+')
 f_coords = open(r'coords.result', 'w+')
 f_relations = open(r'relations.result', 'w+')
 
-
-# output node_coordinate
-# for item in counter.wayDic.items():
-#     refs = item[-1][-1]
-#     start_node = refs[0]
-#     end_node = refs[-1]
-#     # nodes = str(refs[0]) + " " + str(refs[-1])
-#     # nodes = str(osmid) + " " + str(tags) + " " + str(refs)
-#     start_node_coord = counter.nodeDic.get(start_node)
-#     end_node_coord = counter.nodeDic.get(end_node)
-#     if not start_node_coord:
-#         start_node_coord = counter.coordDic.get(start_node)
-#     else:
-#         start_node_coord = start_node_coord[-1]
-#     if not end_node_coord:
-#         end_node_coord = counter.coordDic.get(start_node)
-#     else:
-#         end_node_coord = end_node_coord[-1]
-#     f_ways.write(str(start_node_coord) + ' ' + str(end_node_coord) + '\n')
-
-
-# output start and end node in a way
-# for item in counter.wayDic.items():
-#     refs = item[-1][-1]
-#     start_node = refs[0]
-#     end_node = refs[-1]
-#     f_ways.write(str(start_node) + ' ' + str(end_node) + '\n')
-
-
-# output all info of ways
-for item in counter.wayDic.items():
-    # print(item)
-    f_ways.write(str(item[1][0]) + '\n')
+# whats data you want to write.
+counter.print_ways_result(f_ways, osmid=True, tag=True, coordinate=True)
 
 for item in counter.nodeDic.items():
     # print(item)
@@ -88,10 +101,8 @@ for item in counter.relationDic.items():
     # print(item)
     f_relations.write(item.__str__() + '\n')
 
+# close the file resource.
 f_ways.close()
 f_nodes.close()
 f_coords.close()
 f_relations.close()
-
-# instantiate counter and parser and start parsing Proto nodes
-
