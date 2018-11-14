a73ee2286748f110474358af0eb60fe4a8233913
leo <laixinming@aisino.com>
2018-11-14 20:13:17 +0800

    增加了统计ways和nodes的tags的功能

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 from imposm.parser import OSMParser
+import json
 
 
 # simple class that handles the parsed OSM data.
 class OSMCounter(object):
-    relationDic = {}
-    coordDic = {}
-    nodeDic = {}
-    wayDic = {}
+    relationDic = {} #{osmid:(tags, refs)}
+    coordDic = {} #{osmid:(lat, lon)}
+    nodeDic = {} #{osmid:(tags, coordinary)}
+    wayDic = {} #{osmid:(tags, refs)}
 
     def get_node_coord(self, osmid):
         node = self.nodeDic.get(osmid)
@@ -54,11 +55,42 @@ class OSMCounter(object):
                     output_file.write(str(node) + ' ')
                 output_file.write('\n')
 
+    def count_tags(self, output_file, type='way'):
+        tagsDict = {} #{key:{value:count}}
+        tagsCountList = [] #[(key,value,count)]
+        if type=='way':
+            objDict = self.wayDic
+        if type=='node':
+            objDict = self.nodeDic
+        for osmid, tags_and_others in objDict.items():
+            tags = tags_and_others[0]
+            for k, v in tags.items():
+                if k not in tagsDict:
+                    tagsDict[k] = {}
+                if v not in tagsDict[k]:
+                    tagsDict[k][v] = 1
+                else:
+                    tagsDict[k][v] += 1
+        for key, value_count in tagsDict.items():
+            for value, count in value_count.items():
+                tagsCountList.append((key, value, count))
+        tagsCountList.sort(key=lambda x: x[-1], reverse=True)
+        for key, value, count in tagsCountList:
+            try:
+                output_file.write(str(key) + '\t\t' + str(value) + '\t\t' + str(count) + '\n')
+            except:
+                if not isinstance(key, int):
+                    key = key.encode('utf8')
+                if not isinstance(value, int):
+                    value = value.encode('utf8')
+                output_file.write(str(key) + '\t\t' + str(value) + '\t\t' + str(count) + '\n')
+
     def ways(self, ways):
         # callback method for ways
         for osmid, tags, refs in ways:
-            if 'highway' in tags:
-                self.wayDic[osmid] = (tags, refs)
+            # if 'highway' in tags:
+                # self.wayDic[osmid] = (tags, refs)
+            self.wayDic[osmid] = (tags, refs)
 
     def nodes(self, nodes):
         # callback method for nodes
@@ -83,17 +115,21 @@ p = OSMParser(concurrency=4, ways_callback=counter.ways, nodes_callback=counter.
 p.parse('Porto.osm.pbf')
 
 # write the counter result to file
-f_ways = open(r'ways.result', 'w+')
-f_nodes = open(r'nodes.result', 'w+')
+# f_ways = open(r'ways.result', 'w+')
+f_ways_tags = open(r'ways_tags.result', 'w+')
+# f_nodes = open(r'nodes.result', 'w+')
+f_nodes_tags = open(r'nodes_tags.result', 'w+')
 f_coords = open(r'coords.result', 'w+')
 f_relations = open(r'relations.result', 'w+')
 
 # whats data you want to write.
-counter.print_ways_result(f_ways, osmid=False, tag=False, coordinate=False)
+# counter.print_ways_result(f_ways, osmid=False, tag=True, coordinate=False)
+counter.count_tags(f_ways_tags, type='way')
+counter.count_tags(f_nodes_tags, type='node')
 
-for item in counter.nodeDic.items():
+# for item in counter.nodeDic.items():
     # print(item)
-    f_nodes.write(item.__str__() + '\n')
+    # f_nodes.write(item.__str__() + '\n')
 
 for item in counter.coordDic.items():
     # print(item)
@@ -104,7 +140,9 @@ for item in counter.relationDic.items():
     f_relations.write(item.__str__() + '\n')
 
 # close the file resource.
-f_ways.close()
-f_nodes.close()
+# f_ways.close()
+f_ways_tags.close()
+# f_nodes.close()
+f_nodes_tags.close()
 f_coords.close()
 f_relations.close()
