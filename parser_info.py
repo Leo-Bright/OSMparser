3b33740d2421a2a3f33f70198ee705e984273f21
Leo Bright <buxingzhexiong@sina.com>
2019-03-26 10:35:25 +0800

    parser_info增加提取所有traffic坐标的功能

--- parser_info.py
+++ parser_info.py
@@ -37,36 +37,48 @@ class OSMCounter(object):
 counter = OSMCounter()
 p = OSMParser(concurrency=4, ways_callback=counter.ways, nodes_callback=counter.nodes,
               coords_callback=counter.coords, relations_callback=counter.relations)
-p.parse('porto/dataset/Porto.osm.pbf')
+p.parse('sanfrancisco/dataset/SanFrancisco.osm.pbf')
 
 # write the highway info to file
-f_highway_info = open(r'porto/info/highway.info', 'w+')
+f_highway_info = open(r'sanfrancisco/info/highway.info', 'w+')
 for item in counter.highwayDic.items():
     f_highway_info.write(item.__str__() + '\n')
 f_highway_info.close()
 
 # write the all ways info to file
-f_ways_info = open(r'porto/info/ways.info', 'w+')
+f_ways_info = open(r'sanfrancisco/info/ways.info', 'w+')
 for item in counter.wayDic.items():
     f_ways_info.write(item.__str__() + '\n')
 f_ways_info.close()
 
 # write the all nodes info to file
-f_nodes_info = open(r'porto/info/nodes.info', 'w+')
+f_nodes_info = open(r'sanfrancisco/info/nodes.info', 'w+')
 for item in counter.nodeDic.items():
     f_nodes_info.write(item.__str__() + '\n')
 f_nodes_info.close()
 
+# write the all traffic info to file
+with open(r'sanfrancisco/info/traffic.json', 'w+') as f_traffic_info:
+    traffic_signlas = {'lat':[], 'lon':[]}
+    for node_id in counter.nodeDic:
+        (tags, coordinary) = counter.nodeDic[node_id]
+        if 'highway' not in tags:
+            continue
+        if 'traffic_signals' in tags['highway']:
+            traffic_signlas['lon'].append(str(coordinary[0]))
+            traffic_signlas['lat'].append(str(coordinary[1]))
+    f_traffic_info.write(json.dumps(traffic_signlas) + '\n')
+
 
 # write the all coords info to file
-f_coords_info = open(r'porto/info/coords.info', 'w+')
+f_coords_info = open(r'sanfrancisco/info/coords.info', 'w+')
 for item in counter.coordDic.items():
     f_coords_info.write(item.__str__() + '\n')
 f_coords_info.close()
 
 
 # write the relation info to file
-f_relations_info = open(r'porto/info/relations.info', 'w+')
+f_relations_info = open(r'sanfrancisco/info/relations.info', 'w+')
 for item in counter.relationDic.items():
     f_relations_info.write(item.__str__() + '\n')
 f_relations_info.close()
