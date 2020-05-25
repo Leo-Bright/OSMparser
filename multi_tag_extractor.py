b20f61b79759b5695b14bfd4d7b463b45d6c144b
Leo Bright <buxingzhexiong@sina.com>
2020-05-25 20:07:58 +0800

    提取不包含crossing和traffic的tag file

--- multi_tag_extractor.py
+++ multi_tag_extractor.py
@@ -47,65 +47,67 @@ tag_without_traffic = ['turning_loop', 'give_way', 'bus_stop', 'turning_circle',
 tag_without_all = ['turning_loop', 'give_way', 'bus_stop', 'turning_circle', 'stop', 'speed_camera', 'motorway_junction', 'mini_roundabout']
 tag_classes = [tag_without_crossing, tag_without_traffic]
 
-# for tag_class in tag_classes:
-#     for city in cities:
-#         p.parse(city)
-#         node_to_tag = {}
-#         # write the multi node tag to json file
-#         path, _ = city.rsplit('/', 1)
-#         all_node = counter.nodeDic
-#         for key in all_node:
-#             node_to_tag[str(key)] = {}
-#             tags, _ = all_node[key]
-#             for k in tags:
-#                 node_to_tag[str(key)][k] = tags[k]
+# extract node_with_crossing.tag file
+for tag_class in tag_classes:
+    for city in cities:
+        p.parse(city)
+        node_to_tag = {}
+        # write the multi node tag to json file
+        path, _ = city.rsplit('/', 1)
+        all_node = counter.nodeDic
+        for key in all_node:
+            node_to_tag[str(key)] = {}
+            tags, _ = all_node[key]
+            for k in tags:
+                node_to_tag[str(key)][k] = tags[k]
+
+        ct, _ = city.split('/', 1)
+        network_file = ct + '/network/' + ct + '.network'
+        output_file = path + '/node_with_' + tag_class[4] + '.tag'
+        with open(output_file, 'w+') as output:
+            with open(network_file, 'r') as f:
+                for line in f:
+                    for node in line.strip().split(' '):
+                        if node not in node_to_tag or 'highway' not in node_to_tag[node]:
+                            output.write(node + ' 0\n')
+                        else:
+                            tags = node_to_tag[node]
+                            highway_tag = tags['highway']
+                            try:
+                                node_tag_index = tag_class.index(highway_tag)
+                            except ValueError:
+                                node_tag_index = -1
+                            output.write(node + ' ' + str(node_tag_index + 1) + '\n')
+
+# extract node_with_none.tag file
+# for city in cities:
+#     p.parse(city)
+#     node_to_tag = {}
+#     # write the multi node tag to json file
+#     path, _ = city.rsplit('/', 1)
+#     all_node = counter.nodeDic
+#     for key in all_node:
+#         node_to_tag[str(key)] = {}
+#         tags, _ = all_node[key]
+#         for k in tags:
+#             node_to_tag[str(key)][k] = tags[k]
 #
-#         ct, _ = city.split('/', 1)
-#         network_file = ct + '/network/' + ct + '.network'
-#         output_file = path + '/node_with_' + tag_class[4] + '.tag'
-#         with open(output_file, 'w+') as output:
-#             with open(network_file, 'r') as f:
-#                 for line in f:
-#                     for node in line.strip().split(' '):
-#                         if node not in node_to_tag or 'highway' not in node_to_tag[node]:
-#                             output.write(node + ' 0\n')
-#                         else:
-#                             tags = node_to_tag[node]
-#                             highway_tag = tags['highway']
-#                             try:
-#                                 node_tag_index = tag_class.index(highway_tag)
-#                             except ValueError:
-#                                 node_tag_index = -1
-#                             output.write(node + ' ' + str(node_tag_index + 1) + '\n')
-
-for city in cities:
-    p.parse(city)
-    node_to_tag = {}
-    # write the multi node tag to json file
-    path, _ = city.rsplit('/', 1)
-    all_node = counter.nodeDic
-    for key in all_node:
-        node_to_tag[str(key)] = {}
-        tags, _ = all_node[key]
-        for k in tags:
-            node_to_tag[str(key)][k] = tags[k]
-
-    ct, _ = city.split('/', 1)
-    network_file = ct + '/network/' + ct + '.network'
-    output_file = path + '/node_with_none.tag'
-    with open(output_file, 'w+') as output:
-        with open(network_file, 'r') as f:
-            for line in f:
-                for node in line.strip().split(' '):
-                    if node not in node_to_tag or 'highway' not in node_to_tag[node]:
-                        output.write(node + ' 0\n')
-                    else:
-                        tags = node_to_tag[node]
-                        highway_tag = tags['highway']
-                        try:
-                            node_tag_index = tag_without_all.index(highway_tag)
-                        except ValueError:
-                            node_tag_index = -1
-                        output.write(node + ' ' + str(node_tag_index + 1) + '\n')
+#     ct, _ = city.split('/', 1)
+#     network_file = ct + '/network/' + ct + '.network'
+#     output_file = path + '/node_with_none.tag'
+#     with open(output_file, 'w+') as output:
+#         with open(network_file, 'r') as f:
+#             for line in f:
+#                 for node in line.strip().split(' '):
+#                     if node not in node_to_tag or 'highway' not in node_to_tag[node]:
+#                         output.write(node + ' 0\n')
+#                     else:
+#                         tags = node_to_tag[node]
+#                         highway_tag = tags['highway']
+#                         try:
+#                             node_tag_index = tag_without_all.index(highway_tag)
+#                         except ValueError:
+#                             node_tag_index = -1
+#                         output.write(node + ' ' + str(node_tag_index + 1) + '\n')
 
 
